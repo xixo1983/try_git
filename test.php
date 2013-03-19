cbe6f6f6e7b900fdd1e03778a8f1aa1ca026d26f
Nirmal <rush2nirmal@gmail.com>
2013-03-19 12:05:45 +0530

    First commit

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nirmal
+ * Date: 19/3/13
+ * Time: 12:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+ echo phpinfo();
